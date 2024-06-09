b05db6e7fc6e34e04a7783f26992b41833b30c4e
Edilson Holanda <edilson_holanda@hotmail.com>
2024-06-09 18:15:17 -0300

    Projeto total

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+
+  
+  // photos = [
+  //   {
+  //     url: 
+  //     'https://png.pngtree.com/background/20230614/original/pngtree-bengal-tiger-face-wallpaper-hd-picture-image_3475392.jpg',
+  //     description: 'Tigre 1'
+  //   },
+  //   {
+  //     url: 
+  //     'https://png.pngtree.com/background/20230614/original/pngtree-tiger-face-wallpaper-picture-image_3530391.jpg',
+  //     description: 'Tigre 2'
+  //   }
+  // ]
+}
